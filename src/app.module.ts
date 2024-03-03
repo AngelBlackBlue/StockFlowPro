54e59097c8ad8f4cc592b847badbd99692c15f4c
AngelBlackBlue <angel.blackbkue@hotmail.com>
2024-03-03 18:01:18 -0300

    BDD tabla customers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { CustomersModule } from './customers/customers.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [AuthModule, CustomersModule],
+  imports: [
+    AuthModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3307,
+      username: 'user_angel',
+      password: 'root',
+      database: 'stock_flow_pro',
+      entities: [__dirname + '/**/entity.ts'],
+      synchronize: true,
+    }),
+    CustomersModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/customers/entities/customer.entity.ts
+++ src/customers/entities/customer.entity.ts
@@ -1,1 +1,32 @@
-export class Customer {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'customers' })
+export class Customer {
+  //   @PrimaryGeneratedColumn({ unique: true, type: 'uuid' })
+  @PrimaryGeneratedColumn()
+  id: string; //uuid [primary key]
+  @Column()
+  firstname: string;
+  @Column()
+  lastname: string;
+  @Column({ nullable: true })
+  description: string; // text;
+  @Column({ unique: true })
+  email: string; // [not null,unique]
+  @Column()
+  phone: string;
+  @Column({ nullable: true })
+  company: string;
+  @Column({ nullable: true })
+  image: string;
+  @Column({ nullable: true })
+  address: string;
+  @Column({ nullable: true })
+  number: number;
+  @Column({ nullable: true })
+  location: string;
+  @Column({ nullable: true })
+  province: string;
+  @Column({ unique: true, type: 'uuid' })
+  userId: string; //[ref: > user.id]
+}
