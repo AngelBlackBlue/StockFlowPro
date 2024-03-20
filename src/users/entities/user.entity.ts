50e9bd992483fadac27b2f43cbdfd313438cfd35
AngelBlackBlue <angel.blackbkue@hotmail.com>
2024-03-20 12:52:17 -0300

    user.entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,41 @@
-export class User {}
+import {
+  Column,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity({ name: 'users' })
+export class User {
+  @PrimaryGeneratedColumn()
+  id: string;
+
+  @Column()
+  firstname: string;
+  @Column()
+  lastname: string;
+  @Column({ unique: true })
+  email: string; // [not null,unique]
+  @Column()
+  password: string;
+  @Column()
+  phone: string;
+  @Column({ nullable: true })
+  company: string;
+  @Column({ nullable: true })
+  image: string;
+  @Column({ nullable: true })
+  cbu: string;
+  @Column({ nullable: true })
+  alias: number;
+  @Column({ nullable: true })
+  cuit: string;
+  @Column({ nullable: true })
+  validator: number;
+  @Column()
+  status: string;
+  @Column({ unique: true, type: 'uuid' })
+  userId: string; //[ref: > user.id]
+  @DeleteDateColumn()
+  deletedAt: Date;
+}
