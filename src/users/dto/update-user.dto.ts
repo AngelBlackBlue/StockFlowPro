e659c88b4e0ef10dcf78419a08bfdabdd674773f
AngelBlackBlue <angel.blackbkue@hotmail.com>
2024-03-20 14:24:58 -0300

    create y updat DTO

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,57 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
+import {
+  IsEmail,
+  IsNumber,
+  IsOptional,
+  IsPhoneNumber,
+  IsString,
+} from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto {
+  @IsString()
+  @IsOptional()
+  firstname?: string;
+
+  @IsString()
+  @IsOptional()
+  lastname?: string;
+
+  @IsEmail()
+  @IsOptional()
+  email?: string;
+
+  @IsString()
+  @IsOptional()
+  password?: string;
+
+  @IsPhoneNumber()
+  @IsOptional()
+  phone?: string;
+
+  @IsString()
+  @IsOptional()
+  company?: string;
+
+  @IsString()
+  @IsOptional()
+  image?: string;
+
+  @IsString()
+  @IsOptional()
+  cbu?: string;
+
+  @IsString()
+  @IsOptional()
+  alias?: string;
+
+  @IsString()
+  @IsOptional()
+  cuit?: string;
+
+  @IsNumber()
+  @IsOptional()
+  validator?: number;
+
+  @IsString()
+  @IsOptional()
+  status?: string;
+}
