eecc7e9f3cee822f65a748e75cb8d9bb018b6723
Angel Suarez <112484606+AngelBlackBlue@users.noreply.github.com>
2024-04-14 19:27:40 -0300

    auth decorator

--- src/auth/guard/roles.guard.ts
+++ src/auth/guard/roles.guard.ts
@@ -1,12 +1,13 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { ROLES_KEY } from '../decorators/roles.decorator';
+import { Role } from '../enum/role.enum';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
   constructor(private readonly reflector: Reflector) {} //permitirá leer los metadatos
   canActivate(context: ExecutionContext): boolean {
-    const roles = this.reflector.getAllAndOverride(ROLES_KEY, [
+    const roles = this.reflector.getAllAndOverride<Role>(ROLES_KEY, [
       context.getHandler(), //busca los roles definidos tanto a nivel de método
       context.getClass(), // como a nivel clases
     ]);
