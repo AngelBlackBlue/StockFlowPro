04f49b261d38704d18c5bbdf58e24891b2c4a983
Angel Suarez <112484606+AngelBlackBlue@users.noreply.github.com>
2024-06-06 13:05:26 -0300

    swagger Auth ok

--- src/customers/customers.controller.ts
+++ src/customers/customers.controller.ts
@@ -14,7 +14,7 @@ import { Auth } from 'src/auth/decorators/auth.decotator';
 import { Role } from 'src/common/enum/role.enum';
 import { ActiveUser } from '../common/decorators/active-user.decorator';
 import { ActiveUserInterface } from '../common/interfaces/active-user.interface';
-import { ApiBearerAuth, ApiCreatedResponse, ApiForbiddenResponse, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
+import { ApiBadRequestResponse, ApiBearerAuth, ApiCreatedResponse, ApiForbiddenResponse, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
 
 @ApiTags('Customers')
 @ApiBearerAuth()
@@ -28,6 +28,7 @@ export class CustomersController {
 
   @Post()
   @ApiCreatedResponse({ description: 'The record has been successfully created.'})
+  @ApiBadRequestResponse({ description: 'Bad Request' })
   @ApiForbiddenResponse({ description: 'Forbidden.'})
   create(
     @Body() createCustomerDto: CreateCustomerDto,
