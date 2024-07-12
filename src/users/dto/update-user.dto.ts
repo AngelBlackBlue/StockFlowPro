a23e6646307ac6b7c890de60e66acc1c131b7a67
Angel Suarez <112484606+AngelBlackBlue@users.noreply.github.com>
2024-07-11 22:31:06 -0300

    Actualizando update user

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,13 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { Transform } from 'class-transformer';
+import { IsString, MinLength } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+
+    @Transform(({ value }) => value.trim())
+    @IsString()
+    @MinLength(5)
+    lastname: string;
+
+}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,10 +3,11 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
-import { AuthModule } from 'src/auth/auth.module';
+import { AuthModule } from '../auth/auth.module';
+import { CloudinaryModule } from '../cloudinary/cloudinary.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), AuthModule],
+  imports: [TypeOrmModule.forFeature([User]), AuthModule, CloudinaryModule],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
