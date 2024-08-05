4a9333c423a10d1e65f93d1205de5f7482a51ad1
Angel Suarez <112484606+AngelBlackBlue@users.noreply.github.com>
2024-08-05 20:01:08 -0300

    separacion de la BDD en un modulo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,30 +1,15 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { CustomersModule } from './customers/customers.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule} from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { CloudinaryModule } from './cloudinary/cloudinary.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        type: 'mysql',
-        host: configService.get<string>('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USER'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_DATABASE'),
-        autoLoadEntities: true,
-        synchronize: true,
-      }),
-      inject: [ConfigService],
-    }),
+    ConfigModule.forRoot({isGlobal: true,}),
+    DatabaseModule,
     AuthModule,
     UsersModule,
     CustomersModule,
