e6379915e7265f7f585876e5e0158c0cb627f63f
Angel Suarez <112484606+AngelBlackBlue@users.noreply.github.com>
2024-11-04 20:32:11 -0300

    create stock dto

--- backend/src/stock/dto/create-stock.dto.ts
+++ backend/src/stock/dto/create-stock.dto.ts
@@ -1,1 +1,28 @@
-export class CreateStockDto {}
+import { IsOptional, IsString, IsNumber } from 'class-validator';
+
+export class CreateStockDto {
+
+    @IsString()
+    sku: string;
+        
+    @IsString()
+    product: string;
+    
+    @IsString()
+    @IsOptional()
+    description?: string;
+
+    @IsNumber()
+    amount: number;
+
+    @IsString()
+    @IsOptional()
+    image?: string;
+
+    @IsNumber()
+    buyPrice: number;
+
+    @IsNumber()
+    @IsOptional()
+    salePrice?: number;
+}
