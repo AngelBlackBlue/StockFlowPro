17eca5267df8f7ac44ffdfbee489b95a2d9dc89c
Angel Suarez <112484606+AngelBlackBlue@users.noreply.github.com>
2024-11-10 18:55:16 -0300

    actualizacion

--- hardhat/contracts/KardexPPP.sol
+++ hardhat/contracts/KardexPPP.sol
@@ -1,16 +1,19 @@
 // SPDX-License-Identifier: UNLICENSED
+
 pragma solidity 0.8.27;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract KardesPPP is ERC721 {
     address public owner;
+    uint256 private nextTokenId;
 
     constructor(string memory name, string memory symbol) ERC721(name, symbol) {
         owner = msg.sender;
     }
-
+    
     struct Product {
+        uint256 tokenId;
         string uuid;
         string sku;
         uint timestamp;
@@ -29,13 +32,7 @@ contract KardesPPP is ERC721 {
         sell
     }
 
-    mapping(uint256 => Product) private productDetails;
-
-    uint256 private tokenIdCounter;
-
-    function getTokenIdCounter() public view returns (uint256) {
-        return tokenIdCounter;
-    }
+    Product[] private products;
 
     event ProductRegistered(
         uint256 tokenId,
@@ -52,59 +49,48 @@ contract KardesPPP is ERC721 {
         address owner
     );
 
+
     function registerBuy(
         string memory uuid,
         string memory sku,
         TypeDetail detail,
         uint input,
         uint unitCost,
-        uint output,
         uint balance,
         uint totalCost,
         uint ppp
     ) public {
         require(msg.sender == owner, "Only the owner can register products");
-
         require(bytes(uuid).length > 0, "UUID is required");
         require(bytes(sku).length > 0, "SKU is required");
+        require(detail == TypeDetail.buy || detail == TypeDetail.initial, "Detail is required");
         require(input > 0, "Input is required");
         require(unitCost > 0, "Unit cost is required");
-        require(output == 0, "Output is 0 for register buy");
-        require(balance >= 0, "Balance is required");
-        require(totalCost >= 0, "Total cost is required");
-        require(ppp >= 0, "PPP is required");
 
-        uint tokenId = tokenIdCounter++;
         uint timestamp = block.timestamp;
+        uint256 tokenId = nextTokenId++;
 
-        productDetails[tokenId] = Product({
+        Product memory newProduct = Product({
+            tokenId: tokenId,
             uuid: uuid,
             sku: sku,
             timestamp: timestamp,
             detail: detail,
             input: input,
             unitCost: unitCost,
-            output: output,
+            output: 0,
             balance: balance,
             totalCost: totalCost,
-            ppp: ppp,
+            ppp: ppp
         });
 
+        products.push(newProduct);
         _safeMint(msg.sender, tokenId);
 
-        emit ProductRegistered(
-            tokenId,
-            uuid,
-            sku,
-            timestamp,
-            detail,
-            input,
-            unitCost,
-            output,
-            balance,
-            totalCost,
-            ppp,
-            msg.sender
-        );
+        emit ProductRegistered(tokenId, uuid, sku, timestamp, detail, input, unitCost, 0, balance, totalCost, ppp, msg.sender);
+    }
+
+    function getAllProducts() public view returns (Product[] memory) {
+        return products;
     }
 }
